54354f1f7216cf3fb7730fb7ddfb3c9df6ad62d6
Xenomorphing79 <gourabb52@gmail.com>
2022-05-04 20:49:45 +0530

    Added filter

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,6 +4,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.db.models import Q
 
 from core.models import Tag, Ingredient, Recipe
 
@@ -23,10 +24,7 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixi
         queryset = self.queryset
         if assigned_only:
             queryset = queryset.filter(recipe__isnull=False)
-
-        return queryset.filter(
-            user=self.request.user
-        ).order_by('-name').distinct()
+        return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create a new object"""
@@ -52,9 +50,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve recipes for the authenticated user"""
-        return self.queryset.filter(user=self.request.user)
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get("ingredients")
+        queryset = self.queryset
+        filters = Q(user=self.request.user)
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            filters &= Q(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            filters &= Q(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(filters).distinct()
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
@@ -89,3 +102,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+    def perform_update(self, serializer):
+        """Update a recipe"""
+        serializer.save(user=self.request.user)
